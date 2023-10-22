0796703ec9dd548e4349108911d86b30c69addb8
Borja Refoyo Ruiz <squallium3@gmail.com>
2023-10-22 14:26:39 +0200

    feat(managers/selenium): testing other browsers

--- cataloger_cli/managers/selenium_manager.py
+++ cataloger_cli/managers/selenium_manager.py
@@ -5,7 +5,6 @@ from builtins import Exception
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -20,12 +19,11 @@ class SeleniumManager:
         self.__port = port
         self.__driver = driver
 
-        self.__chrome_driver: WebDriver = None
-        self.__firefox_driver: WebDriver = None
+        self.__chrome_driver: webdriver = None
+        self.__firefox_driver: webdriver = None
         self.__web_loaded = None
 
-    @property
-    def chrome_driver(self) -> WebDriver:
+    def chrome_driver(self) -> webdriver:
         if not self.__chrome_driver:
             chrome_options = ChromeOptions()
             chrome_options.add_experimental_option("debuggerAddress", f'127.0.0.1:{self.__port}')
@@ -35,24 +33,25 @@ class SeleniumManager:
 
         return self.__chrome_driver
 
-    @property
-    def firefox_driver(self) -> WebDriver:
+    def firefox_driver(self, profile_path='') -> webdriver:
         if not self.__firefox_driver:
             firefox_options = FirefoxOptions()
-            firefox_options.add_argument(f'--remote-debugging-port={self.__port}')
-            firefox_options.add_argument('--marionette-port 50550')
-            firefox_options.add_argument('--connect-existing')
+            # firefox_options.add_argument(f'--remote-debugging-port={self.__port}')
+            # firefox_options.add_argument('--marionette-port 50550')
+            # firefox_options.add_argument('--connect-existing')
+            firefox_options.add_argument(f'-profile')
+            firefox_options.add_argument(profile_path)
 
             self.__firefox_driver = webdriver.Firefox(options=firefox_options)
 
         return self.__firefox_driver
 
     @property
-    def driver(self) -> WebDriver:
+    def driver(self) -> webdriver:
         if self.__driver == 'chrome':
-            return self.chrome_driver
+            return self.chrome_driver()
         elif self.__driver == 'firefox':
-            return self.firefox_driver
+            return self.firefox_driver()
         else:
             raise Exception
 
@@ -63,6 +62,17 @@ class SeleniumManager:
                 logging.info(f'Waiting {seconds} loading seconds')
                 time.sleep(seconds)
             self.__web_loaded = web_url
+
+    def scroll_to_the_end(self, loop=True, sleep_time=1):
+        script = 'window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;'
+        len_of_page = self.chrome_driver.execute_script(script)
+        match = False
+        while not match:
+            last_count = len_of_page
+            time.sleep(sleep_time)
+            len_of_page = self.chrome_driver.execute_script(script)
+            if last_count == len_of_page or loop:
+                match = True
 
     def close(self):
         self.driver.close()
